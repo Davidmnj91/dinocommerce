ceb1ae7bf7ce9f7aaa792895ba649390c95b3b16
DavidMnj91 <david.manja91@gmail.com>
2022-10-11 22:47:44 +0200

    simplify userService

--- apps/api/src/app/user/domain/user.service.ts
+++ apps/api/src/app/user/domain/user.service.ts
@@ -1,24 +1,17 @@
 import { EntityRepository } from '@mikro-orm/core';
 import { InjectRepository } from '@mikro-orm/nestjs';
-import { Injectable, Logger } from '@nestjs/common';
+import { Injectable } from '@nestjs/common';
 import { UserAlreadyRegisteredException } from '../../auth/domain/exception/user-already-registered.exception';
 import { UserNotFoundException } from './exception/user-not-found.exception';
 import { User } from './user.entity';
 
 @Injectable()
 export class UserService {
-  private logger = new Logger(UserService.name);
-
   constructor(@InjectRepository(User) private userRepository: EntityRepository<User>) {}
 
   async findUserById(userId: string): Promise<User> {
-    let user;
+    const user = await this.userRepository.findOne({ $or: [{ email: userId }, { userId }] });
 
-    try {
-      user = await this.userRepository.findOne({ $or: [{ email: userId }, { userId }] });
-    } catch (error) {
-      this.logger.error(error);
-    }
     if (!user) {
       throw new UserNotFoundException(userId);
     }
