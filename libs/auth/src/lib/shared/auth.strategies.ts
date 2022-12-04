1555ba2119d3a85164c4a88c7011bdead2f8e118
DavidMnj91 <david.manja91@gmail.com>
2022-12-04 14:23:55 +0100

    Create Backoffice application

--- libs/auth/src/lib/shared/auth.strategies.ts
+++ libs/auth/src/lib/shared/auth.strategies.ts
@@ -2,6 +2,5 @@ export const EMAIL_PASSWORD_STRATEGY = 'email_password';
 export const GOOGLE_STRATEGY = 'google';
 export const FACEBOOK_STRATEGY = 'facebook';
 export const MICROSOFT_STRATEGY = 'microsoft';
-export const TWITTER_STRATEGY = 'twitter';
 
 export const JWT_STRATEGY = 'jwt';
